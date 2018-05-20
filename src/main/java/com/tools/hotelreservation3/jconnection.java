ccd3f46daaefe765aa17357b5b9dd6f0780c2896
murialnakhli <murtada.alnakhli@live.com>
2018-05-20 17:33:48 +0200

    fixed DB table, updated SignUp interface and connectivty to the propere table to be able SignUp

--- src/main/java/com/tools/hotelreservation3/jconnection.java
+++ src/main/java/com/tools/hotelreservation3/jconnection.java
@@ -17,7 +17,7 @@ public class jconnection {
     public static Connection connectDB(){
         try{
             Class.forName("org.sqlite.JDBC");
-            Connection c = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Marouane\\Documents\\HRSProject\\HotelReservation3\\HRS.sqlite");
+            Connection c = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Murtada\\Desktop\\HotelReservation3\\HRS.sqlite");
             return c;
         }
         catch(Exception e){
