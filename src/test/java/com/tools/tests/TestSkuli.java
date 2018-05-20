1e2196ea596be626e26652ed4e6b02d5fb1141a9
RoutinoDesigns <routinodesigns@outlook.com>
2018-05-20 12:48:34 +0200

    Junit,Skali

---
+++ src/test/java/com/tools/tests/TestSkuli.java
@@ -0,0 +1,63 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tools.tests;
+/*
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.sikuli.scripts.Screen;
+import sikuliapplication.MainWindow;
+import com.tools.hotelreservation3.LogIn;
+/**
+ *
+ * @author Murtada
+ */
+/*
+public class TestSkuli {  
+        public void GUIThread(){
+            
+        }
+        @Test
+        public void testGUI(){
+            try{
+                Screen s = new Screen();
+                LogIn l = new LogIn();
+                l.show();
+                
+            }catch(Exception e){
+                
+            }
+        }
+    
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
+
+*/
